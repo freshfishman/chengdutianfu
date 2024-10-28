fc5cc27ebd97e6295ae4d033d12668c2ceb54c6d
juyaocong <juyaocong@163.com>
2024-10-28 18:04:43 +0800

    feat(功能):配置跳转

--- config/config.ts
+++ config/config.ts
@@ -125,7 +125,7 @@ export default defineConfig({
    */
   headScripts: [
     // 解决首次加载时白屏的问题
-    { src: '/scripts/loading.js', async: true },
+    { src: process.env.NODE_ENV === 'production' ? './scripts/loading.js' : '/scripts/loading.js', async: true },
   ],
   //================ pro 插件配置 =================
   presets: ['umi-presets-pro'],
